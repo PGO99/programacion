28fbac712bf26ff483d04218b8997a7758dd7c7e
Pedro <pedrocala2@gmail.com>
2018-12-19 11:03:53 +0100

    ejercicio 1 programación pseint
    Pasando el primer ejercicio de pseint a java, aprendiendo.

---
+++ JAVA/Programación/src/Ejercicios_I_E_1_1/Ejercicio1.java
@@ -0,0 +1,23 @@
+package Ejercicios_I_E_1_1;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String args) throws NumberFormatException, IOException {
+	
+		double nota=0;
+		double media;
+		
+		
+		
+		BufferedReader teclado = new Bufferedreader(new InputStreamReader(System.in));
+
+	
+	
+	
+	 
+	
+		
+	}
+}
