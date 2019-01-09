819201bbce2b6d5883f886448b7f0db1b66de968
PGO99 <43765803+PGO99@users.noreply.github.com>
2019-01-09 12:13:52 +0100

    finalizado ejercicio1Pjava
    finalizado

--- JAVA/Programación/src/Ejercicios_I_E_1_1/Ejercicio1.java
+++ JAVA/Programación/src/Ejercicios_I_E_1_1/Ejercicio1.java
@@ -9,14 +9,50 @@ public class Ejercicio1 {
 
 	public static void main(String args) throws NumberFormatException, IOException {
 	
+		double nota=0;
+		double media;
+		double mediaTotal;
+		boolean Failsafe= true;
+		
+		
 		
 		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
 
 	
-	
-	
-	 
-	
-		
-	}gegreg
-}gegeg
+	do { // estructura para asegurarse de que se recibe el tipo de dato correcto por
+		// consola
+
+	try {grgg
+		System.out.println("Introducir la nota de Programación");
+		nota = Double.parseDouble(teclado.readLine());
+		System.out.println("Introducir la nota de Lenguaje de Marcas");
+		nota += Double.parseDouble(teclado.readLine());
+		System.out.println("Introducir la nota de Bases de Datos");
+		nota += Double.parseDouble(teclado.readLine());
+		System.out.println("Introducir la nota de Entornos de Desarrollo");
+		nota += Double.parseDouble(teclado.readLine());
+		System.out.println("Introducir la nota de Sistemas Informaticos");
+		nota += Double.parseDouble(teclado.readLine());
+		System.out.println("Introducir la nota de Formación y Orientación Laboral");
+		nota += Double.parseDouble(teclado.readLine());
+
+		failsafe = false; // variable de control
+	}
+
+	catch (NumberFormatException ex) {
+
+		System.out.println("No ha introducido un comando valido");
+
+	}
+
+} while (failsafe);
+
+media = (nota) / 6; // calculamos la media
+
+mediaFinal = Math.floor(media * 10) / 10; // reducimos la cantidad de decimales a 1
+
+System.out.println("la nota media es " + mediaTotal);
+
+	}
+
+}
